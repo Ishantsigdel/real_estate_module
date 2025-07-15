0f3c8fbda3449ff27eed7f2f3de57d630633f7d1
IshantSigdel <sigdelishant103@gmail.com>
2025-07-15 20:34:36 +0545

    second commit

--- __manifest__.py
+++ __manifest__.py
@@ -8,14 +8,10 @@
     "depends": ["base"],
     "data": [
         "security/ir.model.access.csv",
-        "views/estate_property_views.xml",
-        "views/estate_menus.xml",
         "views/real_estate_views.xml"
         
     ],
-    "assets": {
-       
-    },
+   
     "installable": True,
     "application": True,
 }
--- models/estate_property.py
+++ models/estate_property.py
@@ -1,5 +1,6 @@
 from odoo import models, fields
 
+
 class RealEstateProperty(models.Model):
     _name = "real.estate.property"
     _description = "Real Estate Property"
@@ -12,17 +13,26 @@ class RealEstateProperty(models.Model):
     selling_price = fields.Float(string="Selling Price")
     bedrooms = fields.Integer(string="Bedrooms", default=2)
     living_area = fields.Integer(string="Living Area (sq ft)")
-    facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage")
+    facades = fields.Integer(string="Number of Facades", default=1)
     garden = fields.Boolean(string="Garden")
     garden_area = fields.Integer(string="Garden Area (sq ft)")
     garden_orientation = fields.Selection(
-        [
-            ('north', 'North'),
-            ('south', 'South'),
-            ('east', 'East'),
-            ('west', 'West')
-        ],
-        string="Garden Orientation"
+        [("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")],
+        string="Garden Orientation",
     )
     last_seen = fields.Datetime(string="Last Seen", default=fields.Datetime.now)
+    active = fields.Boolean(string="Active", default=True)
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        string="Status",
+        required=True,
+        copy=False,
+        default="new",
+    )
