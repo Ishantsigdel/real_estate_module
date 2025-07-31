5fae912d03e9351d483d00b49d48b1f84104c86e
IshantSigdel <sigdelishant103@gmail.com>
2025-07-31 16:27:55 +0545

    add estate account

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Estate Account",
+    "version": "1.0",
+    "author": "Ishant Sigdel",
+    "category": "Accounting",
+    'description': 'Link module between Real Estate and Invoicing',
+
+    "depends": ["estate", "account"],
+    "data": [
+      
+    ],
+    "installable": True,
+    "application": True,
+}
